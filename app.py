458cc0de27232e65b45e1b6ee754388a1601c5b9
LLu0211 <165612537+LLu0211@users.noreply.github.com>
2024-03-31 21:53:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 import plotly
+import plotly.graph_objects as go
 
 theses = pd.read_csv("THESES-TOTAL.csv")
 
